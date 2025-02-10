0271c9e66e416b477250b63adf86c9d30eeefc4c
chazwilson01 <chazwilson01@gmail.com>
2025-02-10 15:05:39 -0600

    Massive Update

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,14 @@ const port = process.env.PORT || 10000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', "https://movie-swiper-59a03.web.app/", "https://movie-swiper-59a03.web.app/login"],
+        origin: ['http://localhost:5173', "https://movie-swiper-59a03.web.app"],
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 });
 
 // Middleware
 app.use(Cors({    
-    origin: ['http://localhost:5173', "https://movie-swiper-59a03.web.app/", "https://movie-swiper-59a03.web.app/login"],
+    origin: ['http://localhost:5173', "https://movie-swiper-59a03.web.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 app.use(express.json());
 app.use('/users', userRoutes);
