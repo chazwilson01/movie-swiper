929d8f79b67aee339426462d85e68c823460d328
chazwilson01 <chazwilson01@gmail.com>
2025-02-11 16:33:20 -0600

    Massive Update

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: ['http://localhost:5173', "https://movie-swiper-59a03.web.app"],
-        methods: ['GET', 'POST', 'PUT', 'DELETE']
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
     }
 });
 
